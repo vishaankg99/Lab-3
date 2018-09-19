dc6cc49e211a9dc5ed43899bf7a478fd7b1c4eda
vishaankg99 <vishaank.ghai@gmail.com>
2018-09-19 18:24:05 -0500

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
+import java.lang.String;
 
 public class Main {
 
@@ -23,6 +24,26 @@ public class Main {
     }
 
     public static void main(String[] unused) {
-        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+
+        String url = "http://erdani.com/tdpl/hamlet.txt";
+        String script;
+        script = urlToString(url);
+        script = script.toLowerCase();
+        String[] scriptSplit = script.split(" ");
+        int count = 0;
+        //for (String val : scriptSplit) {
+        //     System.out.println(val);
+
+        for(int i = 0; i < scriptSplit.length; i++) {
+
+            if (scriptSplit[i].contains("prince")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
     }
 }
+
+
+
